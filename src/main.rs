1aedf4341ec92ca4c6f2357e8a087b035c60271b
Felipe <felipevalerioderamos@gmail.com>
2024-11-22 21:44:32 -0300

    using rodio to play the audio

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use std::{collections::HashMap, fs::File, io::BufWriter};
+
 mod sine_wave;
-use crate::sine_wave::{create_sine_wave, silence_bet};
+use crate::sine_wave::{create_sine_wave, silence_periods, play_audio};
 
 fn main() -> std::io::Result<()> {
     
+
+    // variáveis para a definição de tempo para pontos, traços e de silêncio
+    let dot_time: f32 = 0.1;
+    let dash_time: f32 = dot_time * 3.0;
+    let silence_time = dot_time;
+    let space_time = dot_time * 7.0;
+        
+    // frase e codificação para morse
+    let phrase = "SOS";
+    let phrase_uppercase = phrase.to_uppercase();
+    let mut string_encoded: Vec<&str> = Vec::new();
+    
+    // cria um arquivo binário
+    let file = File::create("sine_wave.bin").unwrap();
+    let mut writer = BufWriter::new(file);
+
+
+    // alfabeto código morse
     let morse_code = HashMap::from([
             ("A", ".-"), ("B", "-..."), ("C", "-.-."), ("D", "-.."),
             ("E", "."), ("F", "..-."), ("G", "--."), ("H", "...."),
@@ -17,23 +36,6 @@ fn main() -> std::io::Result<()> {
     ]);
     
 
-    // variáveis para a definição de tempo para pontos, traços e de silêncio
-    let dot_duration: f32 = 0.1;
-    let dash_duration: f32 = dot_duration * 3.0;
-    let silence_duration = dot_duration;
-    let space_duration = dot_duration * 7.0;
-    
-    // frase e codificação para morse
-    let phrase = "SOS";
-    let phrase_uppercase = phrase.to_uppercase();
-    let mut string_encoded: Vec<&str> = Vec::new();
-
-    // cria um arquivo binário
-    let file = File::create("sine_wave.bin").unwrap();
-    let mut writer = BufWriter::new(file);
-
-
-
 
     for letter in phrase_uppercase.chars() {
 
@@ -43,9 +45,10 @@ fn main() -> std::io::Result<()> {
         if let Some(&value) = morse_code.get(key_str) {
 
             string_encoded.push(value);
-        } else {
-            println!("Erro");
-        }
+        } 
+        // else {
+        //     println!("Erro");
+        // }
     }
 
 
@@ -55,17 +58,18 @@ fn main() -> std::io::Result<()> {
         for symbol in code.chars() {
 
             match symbol {
-                '.' => create_sine_wave(&mut writer, dot_duration)?,
-                '-' => create_sine_wave(&mut writer, dash_duration)?,
+                '.' => create_sine_wave(&mut writer, dot_time)?,
+                '-' => create_sine_wave(&mut writer, dash_time)?,
                 _=> (),
             }
-            let _ = silence_bet(&mut writer, silence_duration);
+            let _ = silence_periods(&mut writer, silence_time);
         }
         
     }
 
-    let _ = silence_bet(&mut writer, space_duration);
+    let _ = silence_periods(&mut writer, space_time);
 
+    play_audio();
 
     Ok(())
 }
--- src/sine_wave.rs
+++ src/sine_wave.rs
@@ -1,12 +1,17 @@
-use std::fs::File;
-use std::io::{BufWriter, Write};
-use std::f32::consts::PI;
 use std::i16::MAX;
+use std::fs::File;
+use std::f32::consts::PI;
+use std::io::{BufReader, BufWriter, Read, Write};
+use rodio::buffer::SamplesBuffer;
+use rodio::{source, OutputStreamHandle};
+use rodio::{OutputStream, source::Source};
 
 
 // const AMPLITUDE: f32 = 1.0;
 const FREQUENCY: f32 = 440.0;
 const SAMPLE_RATE: f32 = 44100.0;
+const CHANNELS: usize = 1;
+const BYTES_PER_SAMPLE: usize = 2;
 
 
 pub fn create_sine_wave(writer: &mut BufWriter<File>, duration: f32) -> std::io::Result<()> {
@@ -23,7 +28,7 @@ pub fn create_sine_wave(writer: &mut BufWriter<File>, duration: f32) -> std::io:
 }
 
 
-pub fn silence_bet(writer: &mut BufWriter<File>, duration: f32) -> std::io::Result<()> {
+pub fn silence_periods(writer: &mut BufWriter<File>, duration: f32) -> std::io::Result<()> {
 
 	let num_samples = (SAMPLE_RATE * duration) as usize;
 
@@ -33,3 +38,52 @@ pub fn silence_bet(writer: &mut BufWriter<File>, duration: f32) -> std::io::Resu
 
 	Ok(())
 }
+
+
+fn raw_pcm_format(buffer: Vec<u8>) -> SamplesBuffer<i16> {
+
+	
+	// Converter os bytes lidos em amostras de i16 (áudio 16-bit)
+	let samples: Vec<i16> = buffer
+		.chunks_exact(BYTES_PER_SAMPLE)
+		.map(|chunk| i16::from_le_bytes([chunk[0], chunk[1]]))
+		.collect();
+	
+	// Criar uma fonte a partir do buffer de amostras
+	let source = SamplesBuffer::new(CHANNELS as u16, SAMPLE_RATE as u32, samples.clone());
+
+	source
+}
+
+
+pub fn play_audio() {
+
+	// inicia o stream de audio
+	let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+	
+	// carrega o arquivo binário
+	let mut file = BufReader::new(File::open("C:/Users/Pichau/Documents/Rust/morse_encode_decode/sine_wave.bin").unwrap());
+	let mut buffer = Vec::new();
+	file.read_to_end(&mut buffer).unwrap();
+
+	let source = raw_pcm_format(buffer);
+
+	// calcula a duração do aúdio
+	// total de samples / (sample_rate (frequencia 44100) * channels (1 ou seja Mono))
+	let total_samples = samples.len() as u64;
+	let duration_secs = total_samples / (SAMPLE_RATE as u64 * CHANNELS as u64);
+
+    let _ = stream_handle.play_raw(source.convert_samples());
+    std::thread::sleep(std::time::Duration::from_secs(duration_secs)); // toca durante o tempo do áudio
+}
+
+
+
+// y(t): valor da onda no tempo tt.
+// A: amplitude da onda, que determina o "tamanho" da onda (ou quão alta ela é).
+// f: frequência da onda em hertz (Hz), que determina quantas oscilações completas ocorrem por segundo.
+// t: tempo.
+// φ: fase inicial, que desloca a onda no tempo.
+
+// y(t)=A⋅sin(2πft+φ) -> 2.0 * PI * FREQUENCY * i as f32 / SAMPLE_RATE).sin();
+// ffplay -f s16le -ar 44100 sine_wave.bin
